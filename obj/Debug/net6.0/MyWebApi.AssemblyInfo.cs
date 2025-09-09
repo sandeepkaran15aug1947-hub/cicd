703e586852796fdd881da358f88489fc095ac917
sandeepkaran <https://github.com/sandeepNoida7>
2025-09-10 02:18:49 +0530

    first commkkilt

--- obj/Debug/net6.0/MyWebApi.AssemblyInfo.cs
+++ obj/Debug/net6.0/MyWebApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyWebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99060121395fcfbd0069a6a4c81ebf70ce451f78")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+269e6734fb424ffecd4d9873dfd09e4f32fd9938")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyWebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyWebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

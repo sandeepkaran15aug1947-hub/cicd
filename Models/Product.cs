3a0bcb1e55b0a29e58736cf11a8d023a5bec5392
sandeepkaran <https://github.com/sandeepNoida7>
2025-09-26 03:14:48 +0530

    tesjjhkkjhkt

--- Models/Product.cs
+++ Models/Product.cs
@@ -2,7 +2,8 @@ namespace MyWebApi.Models
 {
     public class Product
     {
-        public int Id { get; set; }
+        //
+                public int Id { get; set; }
         public string Name { get; set; } = "";
         public decimal Price { get; set; }
     }
